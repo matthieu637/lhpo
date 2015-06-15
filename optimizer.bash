c992ab8335f2ef2d4ee0bdef4e7911dba079fdf6
Matthieu Zimmer <contact@matthieu-zimmer.net>
2015-06-15 10:41:07 +0200

    copy executable on local storage (for cluster run)

--- optimizer.bash
+++ optimizer.bash
@@ -1,7 +1,24 @@
 #!/bin/bash
 
-COMMAND="../../../build/default/exp/kbexploit/RLNNACST_novisu --map ../../../exp/kbexploit/data/arena/small/d_b2_c2.pbm --instances 0 --instances 1 --na 17 --actions ../../../exp/kbexploit/data/actions/human_linear_s15_n17 --state ../../../exp/kbexploit/data/states/final_52human"
-CONFIG_FILE="configRL.ini"
+COMMAND="../../../build/default/exp/kbexploit/RLNNACST_novisu"
+ARGS="--map ../../../exp/kbexploit/data/arena/small/d_b2_c2.pbm --instances 0 --instances 1 --na 17 --actions ../../../exp/kbexploit/data/actions/human_linear_s15_n17 --state ../../../exp/kbexploit/data/states/final_52human"
+CONFIG_FILE="config.ini"
+
+function cpFileFromArgs(){
+        destination=$1
+        args=$2
+
+        read -a array <<< "$args"
+        for element in "${array[@]}"
+        do
+            if [ -e "$element" ] ; then
+                cp $element $destination
+                echo -n "$(basename $element) "
+            else
+                echo -n "$element "
+            fi
+        done
+}
 
 directories=`cat rules.out`
 for dir in $directories ; do
@@ -11,6 +28,7 @@ for dir in $directories ; do
 			
 			#configuration
 			cp $CONFIG_FILE $dir/$setup/
+			hostname >> $dir/$setup/host
 			i=1
 			for parameter in $( head -1 $dir/rules.out ) ; do
 				value=`echo $setup | cut -d'_' -f$i `
@@ -20,7 +38,21 @@ for dir in $directories ; do
 	
 			#run
 			cd $dir/$setup
-			$COMMAND
+			tmp_dir=`mktemp -d`
+			here=`pwd`
+			cp $CONFIG_FILE	$tmp_dir
+			cp $COMMAND $tmp_dir
+			args=$(cpFileFromArgs $tmp_dir "$ARGS")
+			cd $tmp_dir/
+			executable="./$(basename $COMMAND)"
+			chmod +x $executable
+			echo "$executable $args >& full.trace"
+			$executable $args >& full.trace
+			rm $executable
+			
+			cd $here
+			mv $tmp_dir/* .
+			rmdir $tmp_dir
 			cd ../..
 		fi
 	done
