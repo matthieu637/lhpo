417d543182b42897228c41ea91522f0a6068f57a
Matthieu Zimmer <contact@matthieu-zimmer.net>
2015-08-25 14:05:31 +0200

    improve arg of count

--- count.bash
+++ count.bash
@@ -7,15 +7,35 @@ running=0
 empty=0
 finished=0
 
-END_FILE=$(xml sel -t -m "/xml/end_file" -v @value rules.xml)
-
 display_run=0
+remove_running=0
 
-if [[ $# -eq 2 && $2 == "--display-run" ]] ; then
-    display_run=1
-else
-    echo "not displaying the path of running one, if you want it : $0 <path> --display-run"
+if [ ! -e rules.xml ] ; then
+	echo "rules.xml doesn't exists in $1"
+	exit 1
 fi
+
+for arg in "$@"
+do
+	case $arg in
+		"--display-run")
+			display_run=1
+		;;
+		"-h"|"--help")
+			echo "usage $0 : <directory with rules.xml> <options>"
+			echo "options :"
+			echo "	--display-run : displaying the path of still running"
+			echo "	--help : print this message"
+			echo "	--remove-running : remove the running directory"
+			exit 1
+		;;
+		"--remove-running")
+			remove_running=1
+		;;
+	esac
+done
+
+END_FILE=$(xml sel -t -m "/xml/end_file" -v @value rules.xml)
 
 directories=`cat rules.out`
 for dir in $directories ; do
@@ -26,6 +46,9 @@ for dir in $directories ; do
 		elif [[ -e $dir/$setup && ! -e $dir/$setup/$END_FILE ]] ; then
 			running=`expr $running + 1`
 #			cat $dir/$setup/host
+			if [ $remove_running -eq 1 ] ; then
+				rm -rf $dir/$setup
+			fi
                         if [ $display_run -eq 1 ] ; then
                             echo $setup
                         fi
