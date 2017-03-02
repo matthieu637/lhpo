e710eba8f2097e46abbb18489b2972e160bb79ad
Matthieu Zimmer <contact@matthieu-zimmer.net>
2017-03-02 23:25:19 +0100

    allows to define a parameter for a specific config file when multiples config files are given

--- optimizer.bash
+++ optimizer.bash
@@ -112,9 +112,18 @@ function thread_run(){
 	i=1
 	for parameter in $parameters ; do
 		value=`echo $setup | cut -d'_' -f$i `
-		for configf in $CONFIG_FILES ; do
+
+		#if parameter is file specific
+		if [ $(echo $parameter | grep -e ':' | wc -l) -eq 1 ] ; then
+			configf=$(echo $parameter | cut -d ':' -f1)
+			parameter=$(echo $parameter | cut -d ':' -f2)
 			sed -i "s/^\($parameter=\)[0-9,:.truefalse]*$/\1$value/g" $dir/$setup/$configf
-		done
+			
+		else
+			for configf in $CONFIG_FILES ; do
+				sed -i "s/^\($parameter=\)[0-9,:.truefalse]*$/\1$value/g" $dir/$setup/$configf
+			done
+		fi
 		i=`expr $i + 1`
 	done
 	
